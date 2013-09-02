747084af1127828ef1ec6eaba2a0b8887bda4a99
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-02 09:51:13 +0200

    Initial import

---
+++ parse_mocassin.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import numpy as np
+import os
+from yt.frontends.stream.api import load_uniform_grid
+
+
+def _valid_gridfile(path, fname):
+    if not os.path.isfile(os.path.join(path, fname)):
+        raise OSError("Mocassin3D \"%s\" not found in %s" % (fname, path))
+    return os.path.join(path, fname)
+
+
+def _parse_grid0(path):
+    with open(_valid_gridfile(path, 'grid0.out'), 'r') as fd:
+        fd.readline()
+        data = fd.readline().split()
+    return map(np.int64, data[:3]), map(np.float64, data[-2:])
+
+
+def _parse_grid1(path, domain_dimensions):
+    temp_e, dens_e, dens_h = np.loadtxt(_valid_gridfile(path, 'grid1.out'),
+                                        unpack=True)
+    for array in (temp_e, dens_e, dens_h):
+        array.shape = tuple(domain_dimensions)
+    return dict(Density=dens_h + 1e-50,
+                Electron_Temperature=temp_e,
+                Electron_Density=dens_e)
+
+
+def _parse_plotout(path, domain_dims):
+    data = {}
+    lines = np.loadtxt(_valid_gridfile(path, 'plot.out'), dtype=np.float64)
+    for lno in range(1, lines.shape[1]):
+        data['Emission_Line_%i' % lno] = lines[:, lno].reshape(domain_dims)
+    return data
+
+
+def load_mocassin(path):
+    if not os.path.isdir(path):
+        raise OSError("%s is not a valid directory" % path)
+
+    domain_dims, domain_edges = _parse_grid0(path)
+    data = _parse_grid1(path, domain_dims)
+    data.update(_parse_plotout(path, domain_dims))
+    bbox = np.tile(np.array(domain_edges), [1, 3])
+    bbox.shape = (3, 2)
+    return load_uniform_grid(data, np.array(domain_dims), 1, bbox=bbox)
+
+
+if __name__ == "__main__":
+    pf = load_mocassin('/tmp/mokasyn')
+    from yt.mods import ProjectionPlot
+    c = pf.domain_center
+    c[2] = 0.0
+    for field in pf.h.field_list:
+        slc = ProjectionPlot(pf, 'z', field, center=c)
+        slc.save()
